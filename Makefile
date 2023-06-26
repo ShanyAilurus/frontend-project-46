f562320860dc8043c63e22cc683d13390f2f7abe
Nataliia Selianska <natari.sev@gmail.com>
2023-06-26 17:19:15 +0200

    step 4 and fix step 2

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 install:
 	npm ci
+
+gendiff:
+	node bin/gendiff.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint
+
---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const files = [['file1.json', 'file2.json']];
+
+test.each(files)('gendiff', (file1, file2) => {
+  const file1path = getFixturePath(file1);
+  const file2path = getFixturePath(file2);
+  const result = readFile('resultJson.txt');
+  expect(genDiff(file1path, file2path)).toEqual(result);
+});

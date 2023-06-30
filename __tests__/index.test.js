3d3cf3d05deead70b9ff54f2ebc1932d64b9fa72
Nataliia Selianska <natari.sev@gmail.com>
2023-06-30 15:32:50 +0200

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import fs from 'fs';
-import genDiff from '../index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
